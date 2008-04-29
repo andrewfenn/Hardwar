b18e407fda7f70bdd894f54aa63368b75ce37e93
Andrew Fenn <andrewfenn@gmail.com>
2008-04-29 19:04:08 +0100

    - Changed the code around so that we now have a game state manager to work from

--- trunk/include/Main.h
+++
@@ -1,18 +0,0 @@
-
-#ifndef __HW_MAIN_H__
-#define __HW_MAIN_H__
-
-#include "Game.h"
-#include "ExampleApplication.h"
-
-class Main : public ExampleApplication {
-public:
-	Main() { }
-	virtual ~Main() { }
-protected:
-	void createCamera();
-	void chooseSceneManager();
-	void createScene();	void createFrameListener();
-};
-
-#endif
--- trunk/src/Main.cpp
+++ trunk/src/Main.cpp
@@ -1,56 +1,29 @@
-#include "Main.h"
+#include <OgreException.h>
 
-void Main::createCamera() {
-	// create camera, but leave at default position
-	mCamera = mSceneMgr->createCamera("PlayerCam"); 
-   mCamera->setNearClipDistance(5);
-}
+#include "GameManager.h"
+#include "IntroState.h"
 
-void Main::chooseSceneManager() {
-	mSceneMgr = mRoot->createSceneManager(ST_EXTERIOR_CLOSE);
-}
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT ) {
+#else
+int main( int argc, char **argv ) {
+#endif
+    GameManager *gameManager = GameManager::getSingletonPtr();
 
-void Main::createScene() {
-		// setup sky and terrain
-		ColourValue fadeColour(0.4, 0.4, 0.9);
-        mWindow->getViewport(0)->setBackgroundColour(fadeColour);
-        mSceneMgr->setFog(FOG_LINEAR, fadeColour, 0.0, 50, 7000);
+    try {
+        // Initialise the game and switch to the first state
+        gameManager->startGame( IntroState::getSingletonPtr() );
+    }
+    catch ( Ogre::Exception& ex ) {
+        #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+                MessageBox( NULL, ex.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL );
+        #else
+                std::cerr << "An exception has occured: " << ex.getFullDescription();
+        #endif
+    }
 
-        mSceneMgr->setWorldGeometry("hardwar/terrain.cfg");
-        mSceneMgr->setSkyBox(true, "Examples/SpaceSkyBox");
-
-		mSceneMgr->setAmbientLight(ColourValue(0.25, 0.25, 0.25));
-
-		// setup some light
-		Light *light = mSceneMgr->createLight("Light1");
-        light->setType(Light::LT_POINT);
-        light->setPosition(Vector3(1000, 10000, 1000));
-        light->setDiffuseColour(ColourValue::White);
-        light->setSpecularColour(ColourValue::White);
-
-		// setup everything else
-		
-        Entity *ent = mSceneMgr->createEntity( "Moth", "razor.mesh" );
-        SceneNode *node = mSceneMgr->getRootSceneNode()->createChildSceneNode( "MothNode" );
-        node->attachObject( ent );
-
-        // Create the scene node
-        node = mSceneMgr->getRootSceneNode()->createChildSceneNode("CamNode1");
-        //node->yaw(Degree(-45));
-
-		// place our camera in the crater
-		mCamera->setPosition( Vector3( 4000,1000,4000));
-		node->attachObject(mCamera);
-
-        // create the second camera node
-     //   node = mSceneMgr->getRootSceneNode()->createChildSceneNode("CamNode2", Vector3(0, 200, 400));
-}
-
-void Main::createFrameListener()
-{
-	// Create the FrameListener
-   mFrameListener = new Game(mWindow, mCamera, mSceneMgr);
-	mRoot->addFrameListener(mFrameListener);
-   // Show the frame stats overlay
-   mFrameListener->showDebugOverlay(true);
+    delete gameManager;
+    return 0;
 }
