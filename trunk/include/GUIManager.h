ffe188dbd07dec20b480b357bd012bef4a512355
Andrew Fenn <andrewfenn@gmail.com>
2008-10-13 23:52:12 +0700

    Got rid of the the memory macros code

--- trunk/include/GUIManager.h
+++ trunk/include/GUIManager.h
@@ -19,24 +19,11 @@
 #ifndef GUIManager_H
 #define GUIManager_H
 
-#ifndef _CEGUI_fix_h_
-	#define _CEGUI_fix_h_
-	#pragma warning (push)
-	#pragma warning( disable : 4267 )
-	/** This file eases an issue CEGUI has with OGRE's memory manager. Simply include this in
-		any files that are going to be doing 'CEGUI stuff', rather than CEGUI.h, OgreCEGUIRender.h, 
-		OgreCEGUIResourceProvider.h, and OgreCEGUITexture.h (etc). You will be in for smooth(er) 
-		sailing.
-	**/ 
-
-	#include <OgreNoMemoryMacros.h>
-	#include <CEGUI/CEGUI.h>
-	#include <OgreCEGUIRenderer.h>
-	#include <OgreCEGUIResourceProvider.h>
-	#include <OgreCEGUITexture.h>
-	#include <OgreMemoryMacros.h>
-	#pragma warning (pop)
-#endif
+#include <CEGUI/CEGUI.h>
+#include <OgreCEGUIRenderer.h>
+#include <OgreCEGUIResourceProvider.h>
+#include <OgreCEGUITexture.h>
+#include <OgreMemoryMacros.h>
 
 class GUIManager {
 public:
