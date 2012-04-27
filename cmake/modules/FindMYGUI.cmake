b651804c6efa8febd3eb286301ee0bc68c4fe0e2
Andrew Fenn <andrewfenn@gmail.com>
2012-04-28 00:49:26 +0700

    More cleanup of cmake scripts

--- cmake/modules/FindMYGUI.cmake
+++ cmake/modules/FindMYGUI.cmake
@@ -5,10 +5,11 @@
 #  MYGUI_LIB_DIR, the location of the libraries
 #  MYGUI_FOUND, If false, do not try to use MYGUI
 #
-# Copyright © 2009-2010, Andrew Fenn
+# Copyright © 2009-2012, Andrew Fenn
 #
 
-get_filename_component(windows_extras ${CMAKE_SOURCE_DIR}/../hardwar-deps/MyGUI ABSOLUTE)
+
+findpkg_begin(MYGUI)
 
 set(paths
  ~/Library/Frameworks
@@ -19,7 +20,6 @@ set(paths
   /opt/local # DarwinPorts
   /opt/csw # Blastwave
   /opt
-  ${windows_extras}
   )
 
   set(prefixes lib64 lib libs64 libs libs/Win32 libs/Win64)
@@ -36,15 +36,20 @@ FIND_PATH(MYGUI_INCLUDE_DIR MyGUI.h
   PATHS ${paths} 
 )
 
-FIND_LIBRARY(MYGUI_ENGINE_REL NAMES ${release_engine_libs} HINTS $ENV{MYGUIDIR} PATH_SUFFIXES ${prefixes} PATHS ${paths})
-FIND_LIBRARY(MYGUI_ENGINE_DBG NAMES ${debug_engine_libs}   HINTS $ENV{MYGUIDIR} PATH_SUFFIXES ${prefixes} PATHS ${paths})
-FIND_LIBRARY(MYGUI_LIBRARY_REL NAMES ${release_libs} HINTS $ENV{MYGUIDIR} PATH_SUFFIXES ${prefixes} PATHS ${paths})
-FIND_LIBRARY(MYGUI_LIBRARY_DBG NAMES ${debug_libs}   HINTS $ENV{MYGUIDIR} PATH_SUFFIXES ${prefixes} PATHS ${paths})
-set(MYGUI_LIBRARY optimized "${MYGUI_LIBRARY_REL};${MYGUI_ENGINE_REL}" debug "${MYGUI_LIBRARY_DBG};${MYGUI_ENGINE_DBG};")
+set(MYGUI_LIBRARY_NAMES "MYGUI")
+get_debug_names(MYGUI_LIBRARY_NAMES)
+find_library(MYGUI_LIBRARY_REL NAMES ${MYGUI_LIBRARY_NAMES} HINTS ${MYGUI_LIBRARY_DIR_REL} PATH_SUFFIXES "" "release" "relwithdebinfo" "minsizerel")
+find_library(MYGUI_LIBRARY_DBG NAMES ${MYGUI_LIBRARY_NAMES_DBG} HINTS ${MYGUI_LIBRARY_DIR_DBG} PATH_SUFFIXES "" "debug")
+make_library_set(MYGUI_LIBRARY)
+findpkg_finish(MYGUI)
+if (MYGUI_FOUND)
+    # find binaries
+    if (NOT MYGUI_STATIC)
+        if (WIN32)
+            find_file(MYGUI_BINARY_REL NAMES "MyGUI.dll" HINTS ${paths} PATH_SUFFIXES "" bin bin/release bin/relwithdebinfo bin/minsizerel release)
+            find_file(MYGUI_BINARY_DBG NAMES "MyGUI_d.dll" HINTS ${paths} PATH_SUFFIXES "" bin bin/debug debug)
+        endif()
+        mark_as_advanced(MYGUI_BINARY_REL MYGUI_BINARY_DBG)
+    endif()
+endif()
 
-set(MYGUI_FOUND False)
-if (MYGUI_LIBRARY_REL AND MYGUI_INCLUDE_DIR)
-	  set(MYGUI_FOUND True)
-     get_filename_component(MYGUI_LIBRARY_DIR_REL "${MYGUI_LIBRARY_REL}" PATH)
-     set(MYGUI_LIBRARY_DIRS ${MYGUI_LIBRARY_DIR_REL})
-endif ()
--- cmake/modules/FindOIS.cmake
+++ cmake/modules/FindOIS.cmake
@@ -1,56 +1,50 @@
-# Find OIS includes and library
+#-------------------------------------------------------------------
+# This file is part of the CMake build system for OGRE
+#     (Object-oriented Graphics Rendering Engine)
+# For the latest info, see http://www.ogre3d.org/
 #
-# This module defines
-#  OIS_INCLUDE_DIR
-#  OIS_LIB_DIR, the location of the libraries
-#  OIS_FOUND, If false, do not try to use OIS
-#
-# Copyright © 2009-2010, Andrew Fenn
-#
+# The contents of this file are placed in the public domain. Feel
+# free to make use of it in any way you like.
+#-------------------------------------------------------------------
 
-get_filename_component(windows_extras ${CMAKE_SOURCE_DIR}/../hardwar-deps/Ogre ABSOLUTE)
+# - Try to find OIS
+# Once done, this will define
+#
+#  OIS_FOUND - system has OIS
+#  OIS_INCLUDE_DIRS - the OIS include directories 
+#  OIS_LIBRARIES - link these to use OIS
 
-FIND_PATH(OIS_INCLUDE_DIR OIS.h
-  HINTS
-  $ENV{OISDIR}
-  PATH_SUFFIXES include/OIS include/OIS include
-  PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /sw # Fink
-  /opt/local # DarwinPorts
-  /opt/csw # Blastwave
-  /opt
-  C:/OgreSDK
-  C:/Ogre
-  C:/OGRE
-  ${windows_extras}
+include(FindPkgMacros)
+findpkg_begin(OIS)
+
+# Get path, convert backslashes as ${ENV_${var}}
+getenv_path(OIS_HOME)
+getenv_path(OGRE_BYATIS)
+
+# construct search paths
+set(OIS_PREFIX_PATH ${OIS_HOME} ${ENV_OIS_HOME} 
+  ${OGRE_BYATIS}/sdk ${ENV_OGRE_BYATIS}/sdk/include ${ENV_OGRE_BYATIS}/sdk/lib ${ENV_OGRE_BYATIS})
+create_search_paths(OIS)
+# redo search if prefix path changed
+clear_if_changed(OIS_PREFIX_PATH
+  OIS_LIBRARY_FWK
+  OIS_LIBRARY_REL
+  OIS_LIBRARY_DBG
+  OIS_INCLUDE_DIR
 )
 
-FIND_LIBRARY(OIS_LIBRARY 
-  NAMES OIS libOIS OIS.lib
-  HINTS
-  $ENV{OISDIR}
-  PATH_SUFFIXES lib64 lib libs64 libs libs/Win32 libs/Win64 lib/release
-  PATHS
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /sw
-  /opt/local
-  /opt/csw
-  /opt
-   C:/OgreSDK
-   C:/Ogre
-   C:/OGRE
-   ${windows_extras}
-)
+set(OIS_LIBRARY_NAMES OIS)
+get_debug_names(OIS_LIBRARY_NAMES)
 
+use_pkgconfig(OIS_PKGC OIS)
 
-SET(OIS_FOUND "NO")
-IF(OIS_LIBRARY AND OIS_INCLUDE_DIR)
-  SET(OIS_FOUND "YES")
-ENDIF(OIS_LIBRARY AND OIS_INCLUDE_DIR)
+findpkg_framework(OIS)
+
+find_path(OIS_INCLUDE_DIR NAMES OIS.h HINTS ${OIS_INC_SEARCH_PATH} ${OIS_PKGC_INCLUDE_DIRS} PATH_SUFFIXES OIS)
+find_library(OIS_LIBRARY_REL NAMES ${OIS_LIBRARY_NAMES} HINTS ${OIS_LIB_SEARCH_PATH} ${OIS_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" release relwithdebinfo minsizerel)
+find_library(OIS_LIBRARY_DBG NAMES ${OIS_LIBRARY_NAMES_DBG} HINTS ${OIS_LIB_SEARCH_PATH} ${OIS_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" debug)
+make_library_set(OIS_LIBRARY)
+
+findpkg_finish(OIS)
+add_parent_dir(OIS_INCLUDE_DIRS OIS_INCLUDE_DIR)
+
