1171450d4a9a388fedab1258f6be88d9379eef5e
Andrew Fenn <andrewfenn@gmail.com>
2012-09-30 16:13:15 +0700

    Fixed the build script as that you no longer have to link the source code files in both the unit test and the subdirectory in which they originally reside.

---
+++ cmake/scripts/SourceFiles.cmake
@@ -0,0 +1,10 @@
+
+macro(source_files name)
+    set(_temp_source_files "")
+    foreach(src_file ${${name}})
+        string(STRIP ${src_file} src_file)
+        set("_temp_source_files" "${_temp_source_files}${CMAKE_CURRENT_SOURCE_DIR}/${src_file};")
+    endforeach()
+    set(${name} ${_temp_source_files})
+endmacro(source_files)
+
